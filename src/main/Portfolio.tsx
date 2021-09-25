64f3712cd6455c067e0df061ca22b15271382d87
Daniel Reynolds <danielreynolds101@gmail.com>
2021-09-25 13:37:24 -0600

    prog

--- src/main/Portfolio.tsx
+++ src/main/Portfolio.tsx
@@ -7,12 +7,17 @@ import { useState } from 'react';
 import SelfDrivingCar from '../static/images/bobv1.jpg'
 import SelfDrivingCarPlan from '../static/images/bobPlan.jpg'
 import AperPic from "../static/images/aper1.png"
+import FastLoad from "../static/images/fastLoad.gif"
+import Infospaces from "../static/images/infospaces.png"
+import TrueNorthHQ from "../static/images/TrueNorthHQ.jpg"
+import Aper2Pic from "../static/images/aper2.PNG"
 
 
 interface Project {
     content: JSX.Element,
     timeStart: number,
-    name: string
+    name: string,
+    dates: string
 }
 
 export default function Portfolio() {
@@ -20,7 +25,7 @@ export default function Portfolio() {
     const [ordering, setOrdering] = useState('newToOld' as 'newToOld' | 'oldToNew')
 
     const renderProjects = (projectsList: Project[]) => {
-        return projectsList.sort((a, b) => ordering === 'newToOld' ? a.timeStart - b.timeStart : b.timeStart - a.timeStart).map((project) => {
+        return projectsList.sort((a, b) => ordering !== 'newToOld' ? a.timeStart - b.timeStart : b.timeStart - a.timeStart).map((project) => {
             return <Grid container key={project.timeStart}>
                 <Grid item xs={12}>
                     <br />
@@ -30,12 +35,115 @@ export default function Portfolio() {
                         textDecoration: 'none'
                     }}>
                         <Typography variant="h5" color="textPrimary">{project.name}</Typography>
-                        </a>
-                    <br />
+                    </a>
+                    <Typography variant="caption">{project.dates}</Typography>
                     {project.content}
                 </Grid>
             </Grid >
         })
+    }
+
+    const trueNorth: Project = {
+        content: (
+            <Grid container>
+                <Grid container alignItems="center">
+                    <Grid item xs={12} md={6}>
+                        <img src={TrueNorthHQ} style={{
+                            width: '50%'
+                        }} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <div className="textPadded">
+                            <Typography align="left">
+                                In the first half of 2019 as I was finishing high school, I decided it was time to put my self-taught Unity skills to work. In high school, I played quite alot
+                                of first person shooter (FPS) games, so I decided that would be the type of game I would make.
+                                <br />
+                                Looking at this project in hindsight, it definetly is a bit silly, but hey, I was in high school.
+                                <br />
+                                For this project, I
+                            </Typography>
+                            <div className="MuiTypography-alignLeft MuiTypography-root MuiTypography-body1">
+                                <ul>
+                                    <li>
+                                        Created an entire ~2h playthrough videogame.
+                                    </li>
+                                    <li>
+                                        Designed and Implemented an entire logic system for the game, from destructable environmets to A.I. bots. (Theres alot to talk about here!).
+                                    </li>
+                                    <li>
+                                        Poured 1000+ hours of development into the game.
+                                    </li>
+                                    <li>
+                                        Created procedural generation systems to design worlds for me.
+                                    </li>
+                                </ul>
+                            </div>
+                            <Typography align="left">
+                                I am honestly proud of what I created at such a young age and with so little experience. The game now has over 500+ downloads on itch.io and Steam, which I think is pretty cool.
+                            </Typography>
+                        </div>
+                    </Grid>
+                </Grid>
+                {/* <Grid container alignItems="center">
+                    <Grid item xs={12} md={6}>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Pe9kzq50K_8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <div className="textPadded">
+                            <Typography align="left">
+                                Here is the trailer I created for the game.
+                            </Typography>
+                        </div>
+                    </Grid>
+                </Grid> */}
+            </Grid>
+        ),
+        timeStart: new Date('1/01/2019').valueOf(),
+        name: "True North, Published Video game",
+        dates: 'January 2019 → July 2019'
+    }
+
+    const infospaces: Project = {
+        content: (
+            <Grid container>
+                <Grid container alignItems="center">
+                    <Grid item xs={12} md={6}>
+                        <img src={Infospaces} style={{
+                            width: '100%'
+                        }} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <div className="textPadded">
+                            <Typography align="left">
+                                In the spring of 2020, before I transitioned primarily to research, I worked on a video site specifically for CSU Computer Science Students. For this site, I
+                            </Typography>
+                            <div className="MuiTypography-alignLeft MuiTypography-root MuiTypography-body1">
+                                <ul>
+                                    <li>
+                                        Added short but concise 2-minute videos for entry-level CS students. Topics for these videos included SSH'ing into the CS Department's Machines,
+                                        getting started with the Unix/Linux Shell, and Git.
+                                    </li>
+                                    <li>
+                                        Designed and implemented high-quality features for the website. These included: video deletion, video editing, a playlist system (Which is now a really big deal!), and a
+                                        admin account.
+                                    </li>
+                                    <li>
+                                        During this project, I was able to learn PHP, SQL, ffmpeg, and other monolithic technologies that make up a huge part of systems today.
+                                    </li>
+                                </ul>
+                            </div>
+                            <Typography align="left">
+                                I am super proud of what I accomplished in a short time on this project. Some of my videos now have over 1,000 views, just from CS students at CSU! I also was the creator of our playlist system,
+                                which has functioned flawlessly since I created it.
+                            </Typography>
+                        </div>
+                    </Grid>
+                </Grid>
+            </Grid>
+        ),
+        timeStart: new Date('2/01/2020').valueOf(),
+        name: "Video Creation & Web Developing",
+        dates: 'Febuary 2020 → May 2020'
     }
 
     const urbanSustainability: Project = {
@@ -43,42 +151,55 @@ export default function Portfolio() {
             <Grid container>
                 <Grid container alignItems="center">
                     <Grid item xs={12} md={6}>
-                        <img src={SelfDrivingCar} style={{
+                        <img src={FastLoad} style={{
                             width: '100%'
                         }} />
                     </Grid>
                     <Grid item xs={12} md={6}>
                         <div className="textPadded">
                             <Typography align="left">
-                                For our Introduction to Artificiall Intelligcence Class' term research project, my friend Joe and I decided to go above and beyond.
-                                This project started with one goal: turn some physical thing into a self driving car.
+                                For the past year and a half, my primary work as been on an urban sustainablity project, called <a href="https://urban-sustain.org/" target="_blank">Project Sustain</a> here
+                                at Colorado State Univeristies' Computer Science Department.
+                                <br />
+                                <br />
+                                A great example of what I've been doing is illustrated by this animation, in which 50+ megabytes of county income information is rendered at blisteringly fast speeds onto a Leaflet map, all on a mediocre internet connection. Much
+                                of this acceleration is due to novel hashing techniques which I implemented on the server and client.
+                                <br />
+                                <br />
+                                If you want to hear more about what I've been doing in this project,
+                                <Button variant="contained" color='primary' style={{
+                                    margin: '15px',
+                                    marginLeft: '0'
+                                }} onClick={() => {
+                                    setGlobalState({ page: 'contact' })
+                                }}>Contact Me!</Button>
                             </Typography>
-                            <br />
+                        </div>
+                    </Grid>
+                </Grid>
+                <Grid container alignItems="center">
+                    <Grid item xs={12} md={6}>
+                        <img src={Aper2Pic} style={{
+                            width: '100%'
+                        }} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <div className="textPadded">
                             <Typography align="left">
-                                Wi-fi controlled self driving car kits tend to be very expensive, so we decided to build something from (nearly) scratch. The car itself consists of:
+                                Here is another example of my work, in which flood {'&'} trail data that I curated is being rendered onto a Leaflet map through a visualization system which I designed.
                             </Typography>
-                            <div className="MuiTypography-alignLeft MuiTypography-root MuiTypography-body1">
-                                <ul>
-                                    <li>
-                                        A $20 RC car from Walmart that has been stripped down to its chassis and motors.
-                                    </li>
-                                    <li>
-                                        Two 9v batteries, one for the steering and one for drive.
-                                    </li>
-                                    <li>
-                                        A Raspberry Pi to receive communications from a control server, and relay the controls to the engines.
-                                    </li>
-                                    <li>
-                                        A webcam for eyesight.
-                                    </li>
-                                    <li>
-                                        A ton of wires, hotglue, duct-tape, and popsicle sticks to link everything together.
-                                    </li>
-                                </ul>
-                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12}><br/></Grid>
+                </Grid>
+                <Grid container alignItems="center">
+                    <Grid item xs={12} md={6}>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/7B3mC9E3kK0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <div className="textPadded">
                             <Typography align="left">
-                                Through building the internals of this car, I gained some basic electrical engineering knowledge, an understanding of motor controllers, and a serous appreciaction
-                                for hardware engineers.
+                                Here is a video which I made for the project outlining how to use our services to identify vulnerable places.
                             </Typography>
                         </div>
                     </Grid>
@@ -86,7 +207,8 @@ export default function Portfolio() {
             </Grid>
         ),
         timeStart: new Date('2/02/2020').valueOf(),
-        name: "Urban Sustainabilty Research & Software Engineering"
+        name: "Urban Sustainabilty Research & Software Engineering",
+        dates: 'Febuary 2020 → Present'
     }
 
     const selfDrivingCar: Project = {
@@ -187,18 +309,21 @@ export default function Portfolio() {
             </Grid>
         ),
         timeStart: new Date('8/30/2021').valueOf(),
-        name: "Self Driving Car"
+        name: "Self Driving Car",
+        dates: 'August 2021 → Present'
     }
 
     return <Grid container justify="flex-start">
         <Grid item xs={12} style={{ marginBottom: "2vh" }}>
             <Typography variant='h4'>
-                My Projects.
+                My (Proudest) Projects.
             </Typography>
         </Grid>
         {renderProjects([
             selfDrivingCar,
-            urbanSustainability
+            urbanSustainability,
+            infospaces,
+            trueNorth
         ])}
     </Grid>
 }
